c6697ba92d56c2ad6d6bd9fbb4b8d18c2a0f789b
sunrab1822 <wbarnus99@gmail.com>
2023-11-02 01:14:46 +0100

    opencrates

---
+++ resources/js/components/CrateItem.vue
@@ -0,0 +1,18 @@
+<template>
+       <div class="card col-md-4 m-1">
+            <img class="card-img " :src="`https://media.valorant-api.com/weaponskins/${uuid}/displayicon.png`">
+            <div class="card-img-overlay d-flex align-items-end">
+                <h4 class="card-title">{{ name }}</h4>
+            </div>
+        </div>
+</template>
+
+<script setup>
+    defineProps(['name', 'uuid'])
+
+
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/OpenCrate.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <div class="row">
+        <div class="col">
+            <button class="btn btn-success" >&lsaquo;  Back</button>
+        </div>
+        <div class="col text-center">
+            <h1 class="">{{ crateObj.name }}</h1>
+        </div>
+        <div class="col"></div>
+    </div>
+
+    <div class="d-flex justify-content-center ">
+        <div class="bg-secondary rounded me-2">
+            <button class="btn">1</button>
+            <button class="btn">2</button>
+            <button class="btn">3</button>
+            <button class="btn">4</button>
+            <button class="btn">5</button>
+            <button class="btn">More</button>
+        </div>
+        <div>
+            <button class="btn btn-success">Open</button>
+        </div>
+    </div>
+    <div>
+        <h3>Crate items</h3>
+        <div class="row">
+            <CrateItem v-for="skin in crateObj.contents" :name="skin.name" :uuid="skin.uuid"></CrateItem>
+        </div>
+    </div>
+
+
+</template>
+
+<script setup>
+const prop = defineProps(['crate'])
+
+ let crateObj = JSON.parse(prop.crate)
+    console.log(crateObj);
+
+
+</script>
